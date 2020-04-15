4c6b32f04c1ae142e9b0c2875673723e1e8373bb
AL <Anton_Bertram@gmx.de>
2020-04-15 14:19:32 +0200

    m

--- INF/B4/Verteilte Verarbeitung (VV)/Uebungen/Uebung 1/src/main/java/training/kata/roman/Roman.java
+++ INF/B4/Verteilte Verarbeitung (VV)/Uebungen/Uebung 1/src/main/java/training/kata/roman/Roman.java
@@ -2,8 +2,47 @@ package training.kata.roman;
 
 public class Roman
 {
-    static int romanToArabic(String number)
+    static int romanToArabic(String romanNumber)
     {
-        return 0;
+        char[] romanNumberDigits = romanNumber.toCharArray();
+        int arabicNumber = 0;
+        for (char digit : romanNumberDigits)
+        {
+            arabicNumber += getArabicNumber(digit);
+        }
+
+        return arabicNumber;
+    }
+
+    private static int getArabicNumber(char digit)
+    {
+        int arabicNumber = 0;
+        switch (digit)
+        {
+            case 'I':
+                arabicNumber += 1;
+                break;
+            case 'V':
+                arabicNumber += 5;
+                break;
+            case 'X':
+                arabicNumber += 10;
+                break;
+            case 'L':
+                arabicNumber += 50;
+                break;
+            case 'C':
+                arabicNumber += 100;
+                break;
+            case 'D':
+                arabicNumber += 500;
+                break;
+            case 'M':
+                arabicNumber += 1000;
+                break;
+            default:
+                arabicNumber += 0;
+        }
+        return arabicNumber;
     }
 }
